262ea8313ae772ef16603a8a2379261b8f6cc23b
Janusz Kocik <janusz.kocik@crm.media.pl>
2018-04-13 11:28:23 +0200

    Integrate vue test utils

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    let wrapper = mount(HelloWorld)
+    expect(wrapper.find('h1').text())
       .to.equal('Welcome to Your Vue.js App')
   })
 })
